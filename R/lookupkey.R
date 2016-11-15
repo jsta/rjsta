036374298b28e549a9b269954c023febb52a2d3f
jsta <jstachelek@utexas.edu>
2016-11-15 15:47:18 -0500

    fix sorting bug

--- R/lookupkey.R
+++ R/lookupkey.R
@@ -4,10 +4,10 @@
 #'@param input character vector
 #'@export
 #'@examples
-#'key <- cbind(c("one", "two", "three", "four",
-#'"five"), c("one1", "two2", "three3", "over", "over"))
-#'input <- c("one", "one", "two", "three", "four", "five")
-#'lookupkey(key, input)
+#' key <- cbind(c("one", "two", "three", "four",
+#'           "five"), c("one1", "two2", "three3", "over", "over"))
+#' input <- c("one", "one", "two", "three", "four", "five")
+#' lookupkey(key, input)
 #'
 lookupkey <- function(key, input){
   #add a check for duplicate 'to' key values
@@ -15,9 +15,9 @@ lookupkey <- function(key, input){
   input_length <- length(input)
   
   key <- data.frame(key, stringsAsFactors = FALSE)
-  input <- data.frame(V1 = input, stringsAsFactors = FALSE)
+  input <- data.frame(X1 = input, stringsAsFactors = FALSE)
   
-  output <- merge(key, input, all.y = TRUE)[,2]
+  output <- merge(input, key, all.x = TRUE, all.y = FALSE, sort = FALSE)[,2]
   output[is.na(output)] <- merge(key, input, all.y = TRUE)[which(is.na(output)),1]
   
   if(input_length != length(output)){
