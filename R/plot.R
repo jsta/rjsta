10be52c78a67a149e53d57d93f0f91edb59e0e79
jsta <jstachelek@utexas.edu>
2016-11-15 16:07:14 -0500

    fix plot_invisible, pass devtools::check with no errors

--- R/plot.R
+++ R/plot.R
@@ -2,18 +2,20 @@
 #' plot_invisible
 #' @description Wrapper to silently create a plot
 #' @details Stores plots as image files in the temporary workspace (See <http://stackoverflow.com/questions/20363266/how-can-i-suppress-the-creation-of-a-plot-while-calling-a-function-in-r>)
-#' @param ... character plotting code to be evaluated
+#' @param text character plotting code to be evaluated
+#' @param outpath file.path
+#' @importFrom grDevices png dev.off
 #' @export
 #'
-#' @examples
-#' plot_invisible("plot(1)")
-plot_invisible <- function(...){
-  browser()
-  ff <- tempfile()
+#' @examples \dontrun{
+#' plot_invisible("plot(1)", "test.png")
+#' }
+plot_invisible <- function(text, outpath){
+  ff <- outpath
   png(filename = ff)
-  res <- eval(parse(text = ...))
+  res <- eval(parse(text = text))
   dev.off()
-  unlink(ff)
+  # unlink(ff)
   res
 }
 
