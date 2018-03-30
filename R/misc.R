108547b6ac0ef132e2df3b2095dcabbbc49fcdb7
jsta <stachel2@msu.edu>
2018-03-30 15:52:42 -0400

    add gitignore fxn

--- R/misc.R
+++ R/misc.R
@@ -7,3 +7,25 @@ Mode <- function(x){
   ux <- unique(x)
   ux[which.max(tabulate(match(x, ux)))]
 }
+
+#' gitignore
+#' 
+#' @param f file.path or wildcard to add to gitignore
+#' 
+#' @export
+#' @examples \dontrun{
+#' gitignore("test")
+#' }
+gitignore <- function(f, dry.run = FALSE){
+  message("Before:")
+  current <- readLines(".gitignore")
+  print(current)
+  
+  message("\n After:")
+  if(!dry.run & !(f %in% current)){
+    print(c(current, f))
+    write(f, file = ".gitignore", append = TRUE)
+  }else{
+    print(current)
+  }
+}
