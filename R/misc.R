970feb25fb944ffc9c43a93baca57e1b58057925
jsta <stachel2@msu.edu>
2018-04-30 15:04:33 -0400

    rebuild roxygen

--- R/misc.R
+++ R/misc.R
@@ -11,21 +11,24 @@ Mode <- function(x){
 #' gitignore
 #' 
 #' @param f file.path or wildcard to add to gitignore
+#' @param dry.run logical 
+#' @param verbose logical, print operation results?
 #' 
 #' @export
 #' @examples \dontrun{
 #' gitignore("test")
 #' }
-gitignore <- function(f, dry.run = FALSE){
+gitignore <- function(f, dry.run = FALSE, verbose = FALSE){
   message("Before:")
   current <- readLines(".gitignore")
-  print(current)
+  
+  if(verbose){print(current)}
   
   message("\n After:")
   if(!dry.run & !(f %in% current)){
-    print(c(current, f))
+    if(verbose){print(c(current, f))}
     write(f, file = ".gitignore", append = TRUE)
   }else{
-    print(current)
+    if(verbose){print(current)}
   }
 }
