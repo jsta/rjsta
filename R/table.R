ef3d514efb22b27e9a2f88079ff960d4333e9409
jsta <stachel2@msu.edu>
2019-06-03 13:31:36 -0400

    add pdf_table function, closes #5, closes #4

---
+++ R/table.R
@@ -0,0 +1,30 @@
+#' pdf_table
+#' 
+#' @description Create a pdf snapshot of a data.frame or kable output
+#' @param x data.frame or kable output
+#' @param out_name character file name
+#' @importFrom gridExtra grid.table
+#' @importFrom grDevices pdf
+#' @export
+#' @examples \dontrun{
+#' data(iris)
+#' pdf_table(iris[1:5,])
+#' pdf_table(knitr::kable(iris[1:5,]))
+#' }
+pdf_table <- function(x, out_name = "test.pdf"){
+  if(is.data.frame(x)){
+    pdf(file = out_name)
+    gridExtra::grid.table(x, rows = rep("", nrow(x)))
+    dev.off()
+  }
+  if(is.character(x)){
+    zz <- file("test.md", "w")
+    sink(zz)
+    cat("\\pagenumbering{gobble}")
+    print(x)
+    sink()
+    close(zz)
+    system("pandoc -s test.md -o test.pdf")
+  }
+}
+
