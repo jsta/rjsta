0c1fc0446ee36a20f59904aa7fbd71130ee5defa
jsta <stachel2@msu.edu>
2019-08-23 12:04:11 -0400

    add state abb

--- R/geo.R
+++ R/geo.R
@@ -50,9 +50,15 @@ point_in_poly <- function(dt, poly_shape){
 #' @param crs projection string
 #' @importFrom sf st_as_sf
 #' @importFrom maps map
+#' @import datasets
 #' @export
 usa_sf <- function(crs){
-  sf::st_as_sf(maps::map("state", plot = FALSE, fill = TRUE))
+  res <- sf::st_as_sf(maps::map("state", plot = FALSE, fill = TRUE))
+  state_key <- data.frame(state = state.abb, 
+                          ID = tolower(state.name), 
+                          stringsAsFactors = FALSE)
+  res <- dplyr::left_join(res, state_key, by = "ID")
+  dplyr::filter(res, !is.na(state))
 }
 
 #' Get sf objects within a source object
