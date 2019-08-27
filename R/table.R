009d8112b83b53e8c89c37e4859c0c0480a1247a
jsta <stachel2@msu.edu>
2019-08-27 11:27:05 -0400

    more detailed example

--- R/table.R
+++ R/table.R
@@ -8,12 +8,13 @@
 #' @export
 #' @examples \dontrun{
 #' data(iris)
-#' pdf_table(iris[1:5,])
-#' pdf_table(knitr::kable(iris[1:5,]), out_name = "zz2.pdf")
+#' pdf_table(iris[rep(1:5, 12),])
+#' pdf_table(knitr::kable(iris[rep(1:5, 12),]), out_name = "zz2.pdf")
 #' }
 pdf_table <- function(x, out_name = "test.pdf"){
   if(is.data.frame(x)){
     pdf(file = out_name)
+    # Only fits 25 rows of data. Warn if greater?
     gridExtra::grid.table(x, rows = rep("", nrow(x)))
     dev.off()
   }
