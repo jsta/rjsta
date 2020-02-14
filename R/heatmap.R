5f86e1f9cf9c4f69328ec85b37b80789add784a2
jsta <stachel2@msu.edu>
2020-02-14 11:11:48 -0500

    add heatmap function, closes #13

---
+++ R/heatmap.R
@@ -0,0 +1,31 @@
+#' heatmap
+#' 
+#' @param dt data.frame
+#' @importFrom corrr correlate
+#' @importFrom pheatmap pheatmap
+#' @importFrom dplyr `%>%`
+#' @importFrom grDevices colorRampPalette
+#' @importFrom RColorBrewer brewer.pal
+#' @examples \dontrun{
+#' data(iris)
+#' heatmap(data.frame(iris))
+#' }
+heatmap <- function(dt){
+  # dt <- iris
+  test <- dt %>%
+    dplyr::select_if(is.numeric) %>%
+    corrr::correlate() %>%
+    data.frame()
+  test_rowname    <- test$rowname
+  test            <- dplyr::select(test, -"rowname")
+  row.names(test) <- test_rowname
+  
+  hmap.palette_red <-  colorRampPalette(
+    RColorBrewer::brewer.pal(n = 7, name = "Reds"))
+  hmap.palette_blue <-  colorRampPalette(
+    RColorBrewer::brewer.pal(n = 7, name = "Blues"))
+  hmap_cols <- rev(c(rev(hmap.palette_red(10)), hmap.palette_blue(10)))
+  
+  pheatmap::pheatmap(t(test),
+           color = hmap_cols)
+}
