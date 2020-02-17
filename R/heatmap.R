52e91fd49da02075c72fe26a5810b1b037781f80
jsta <stachel2@msu.edu>
2020-02-17 17:25:52 -0500

    add option to focus heatmap, closes #14

--- R/heatmap.R
+++ R/heatmap.R
@@ -1,26 +1,34 @@
 #' jheatmap
 #' 
 #' @param dt data.frame
+#' @param focal_columns character vector specifying column names to focus on
 #' @importFrom corrr correlate
 #' @importFrom pheatmap pheatmap
 #' @import dplyr
 #' @importFrom grDevices colorRampPalette
 #' @importFrom RColorBrewer brewer.pal
+#' @importFrom rlang UQ sym
 #' @export
 #' @examples \dontrun{
 #' data(iris)
 #' dt <- iris
 #' jheatmap(dt)
+#' jheatmap(dt, c("Sepal.Length"))
 #' }
-jheatmap <- function(dt){
+jheatmap <- function(dt, focal_columns = NULL){
   # dt <- ggplot2::diamonds
   test <- data.frame(dt) %>%
     dplyr::select_if(is.numeric) %>%
-    corrr::correlate() %>%
+    corrr::correlate(quiet = TRUE) %>%
     data.frame()
+  
   test_rowname    <- test$rowname
   test            <- dplyr::select(test, -"rowname")
   row.names(test) <- test_rowname
+  
+  if(length(focal_columns) > 0){
+    test <- dplyr::select(test, focal_columns)
+  }
   
   hmap.palette_red <-  colorRampPalette(
     RColorBrewer::brewer.pal(n = 7, name = "Reds"))
@@ -41,6 +49,19 @@ jheatmap <- function(dt){
                          hmap.palette_blue(abs(frac_neg))))
     }
   }
-  
-  pheatmap::pheatmap(t(test), color = hmap_cols)
+
+  if(length(focal_columns) == 1){
+    test_rowname <- row.names(test)[order(test[,focal_columns])]
+    test <- arrange(test, rlang::UQ(rlang::sym(focal_columns))) %>%
+      dplyr::filter(!is.na(rlang::UQ(rlang::sym(focal_columns))))
+    test_rowname <- test_rowname[test_rowname != focal_columns]
+    row.names(test) <- test_rowname
+  }
+  if(length(focal_columns) > 0){
+    suppressWarnings(pheatmap::pheatmap(test, color = hmap_cols, 
+                                        cluster_rows = FALSE, 
+                                        cluster_cols = FALSE))
+  }else{
+    pheatmap::pheatmap(t(test), color = hmap_cols)
+  }
 }
