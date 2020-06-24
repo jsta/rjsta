455d9b9273970ac0db62696632c27f14e394a233
jsta <stachel2@msu.edu>
2020-06-24 17:43:14 -0400

    add svg_to_pdf function

---
+++ R/images.R
@@ -0,0 +1,16 @@
+
+#' svg_to_pdf 
+#' 
+#' @param url url
+#' @param outname outname
+#' 
+#' @importFrom rsvg rsvg_pdf
+#' @export 
+#' @examples \dontrun{
+#' svg_to_pdf(
+#' "https://img.shields.io/badge/Paper-10.1002/lno.11137-blue.svg")
+#' }
+svg_to_pdf <- function(url, outname = gsub(".svg", ".pdf", basename(url))){
+  # url <- "https://zenodo.org/badge/doi/10.5281/zenodo.2554212.svg"
+  rsvg::rsvg_pdf(url, file = outname)
+}
