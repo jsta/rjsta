1e904a773f37323f89de2d46e3b37d53e174d681
jsta <stachel2@msu.edu>
2022-06-07 10:43:36 -0600

    linting

--- R/images.R
+++ R/images.R
@@ -1,18 +1,18 @@
 
 #' Convert an svg file to pdf
-#' 
+#'
 #' @param url url
 #' @param outname outname
-#' 
+#'
 #' @importFrom rsvg rsvg_pdf
-#' @export 
+#' @export
 #' @examples \dontrun{
 #' svg_to_pdf(
-#' "https://img.shields.io/badge/Paper-10.1002/lno.11137-blue.svg")
+#'   "https://img.shields.io/badge/Paper-10.1002/lno.11137-blue.svg")
 #' }
-svg_to_pdf <- function(url, outname = gsub(".svg", ".pdf", basename(url))){
+svg_to_pdf <- function(url, outname = gsub(".svg", ".pdf", basename(url))) {
   # url <- "https://zenodo.org/badge/doi/10.5281/zenodo.2554212.svg"
-  if(!file.exists(outname)){
+  if (!file.exists(outname)) {
     rsvg::rsvg_pdf(url, file = outname)
   }
 }
